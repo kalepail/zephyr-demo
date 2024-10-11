fcbc754b30358f047d94ed6192390c4e6a4366b8
Tyler van der Hoeven <hi@tyvdh.com>
2024-10-11 16:21:58 +0100

    small updates

--- run.sh
+++ run.sh
@@ -8,10 +8,15 @@ else
   exit 1
 fi
 
+wasm_path=target/wasm32-unknown-unknown/release/retro_hello_world.wasm
+
 stellar contract build
-stat -f "%z" target/wasm32-unknown-unknown/release/retro_hello_world.wasm
-contract_id=$(stellar contract deploy --wasm target/wasm32-unknown-unknown/release/retro_hello_world.wasm --network testnet --source default)
+stat -f "%z" $wasm_path
+contract_id=$(stellar contract deploy --wasm $wasm_path --network testnet --source default)
 echo "Contract ID: $contract_id"
+
 cargo build --release --target wasm32-unknown-unknown --features mercury
-stat -f "%z" target/wasm32-unknown-unknown/release/retro_hello_world.wasm
-mercury-cli --jwt $JWT --local false --mainnet false retroshade --project "zephyr-demo" --contracts $contract_id --target target/wasm32-unknown-unknown/release/retro_hello_world.wasm
+stat -f "%z" $wasm_path
+mercury-cli --jwt $JWT --local false --mainnet false retroshade --project "zephyr-demo" --contracts $contract_id --target $wasm_path
+
+stellar contract invoke --id $contract_id --network testnet --source default --send yes -- hello --to world
